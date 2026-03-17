9908676ba4f2c62eb605d73c70f9f8960d55049b
PAAWSA02LC1026 <Duoc@PAAWSA02LC1026.duoc.acad>
2026-03-17 11:12:12 -0300

    fronted listo creo

---
+++ fronted/src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+
+function App() {
+  const [planes, setPlanes]   = useState([])
+  const [error, setError]     = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/api/planes").then(resp=>{
+      if(!resp.ok) throw new Error("Error al obtener planes")
+        return resp.json()
+    }).then(data=>{
+      setPlanes(data)
+      setLoading(false)
+    }).catch(err=>{
+      setError(err.message)
+    })
+  },[])
+
+  return (
+    <div>
+      <h1>SPA relax</h1>
+      {loading && <p>Cargando planes...</p> }
+      {error && <p>Error: {error}</p>       }
+
+      <ul>
+        {planes.map(plan => (
+          <li key={plan.id}>
+            <h3>{plan.nombre}</h3>
+            <h3>{plan.descripcion}</h3>
+            <strong>{personalbar.precio}</strong>
+
+
+          </li>
+        ))}
+       
+      </ul>
+    </div>
+
+
+  )
+}
+
+export default App
